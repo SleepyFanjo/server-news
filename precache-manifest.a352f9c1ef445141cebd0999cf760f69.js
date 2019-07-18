f770845b6f903dce10d44dd576bb958355a7ff7e
Quentin Chevrin <qchevrin@pictet.com>
2019-07-18 15:45:00 +0200

    Updates

---
+++ precache-manifest.a352f9c1ef445141cebd0999cf760f69.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab07caca6386314632b593a4e991b49",
+    "url": "/server-news/index.html"
+  },
+  {
+    "revision": "1d3ea156a5dd70759dd7",
+    "url": "/server-news/static/css/main.f8b9d259.chunk.css"
+  },
+  {
+    "revision": "cb3172cd3b50fb1f7f2b",
+    "url": "/server-news/static/js/2.2b91e2f1.chunk.js"
+  },
+  {
+    "revision": "1d3ea156a5dd70759dd7",
+    "url": "/server-news/static/js/main.c0543e4e.chunk.js"
+  },
+  {
+    "revision": "bbf885703da7047769c3",
+    "url": "/server-news/static/js/runtime~main.567dc47f.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/server-news/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/server-news/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/server-news/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/server-news/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "824c31a5caefd5ca8bdffb1b1b9843e2",
+    "url": "/server-news/static/media/background.824c31a5.png"
+  }
+]);
